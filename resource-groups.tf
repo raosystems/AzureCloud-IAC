b10e8409263a500323ca8e0283a3e003d8b62be4
Amit Kumar <amitrao2026ce@hotmail.com>
2026-06-26 23:11:00 +0530

    resource groups modified

--- resource-groups.tf
+++ resource-groups.tf
@@ -1,11 +1,27 @@
-resource "azurerm_resource_group" "rg" {
+## Resource Group for Virtual Machines
+resource "azurerm_resource_group" "rg1_for_vm" {
   name     = var.resource_group_name_for_vm
-  location = var.resource_group_location
+  location = var.resource_location_east_us_2
 
   tags = {
     Environment = var.Environment
     ManagedBy   = var.ManagedBy
     CreatedBy   = var.CreatedBy
     CostCenter  = var.CostCenter
+    ResourceLocation = var.resource_location_east_us_2
+  }
+}
+
+## Resource Group for Storage
+resource "azurerm_resource_group" "rg1_for_storage" {
+  name     = var.resource_group_name_for_storage
+  location = var.resource_location_east_us_2
+
+  tags = {
+    Environment = var.Environment
+    ManagedBy   = var.ManagedBy
+    CreatedBy   = var.CreatedBy
+    CostCenter  = var.CostCenter
+    ResourceLocation = var.resource_location_east_us_2
   }
 }
