2612f9db954e333fcc9bf9693ebe4213968872a1
Amit Kumar <amitrao2026ce@hotmail.com>
2026-06-27 10:12:18 +0530

    modified terraform providers block

--- terraform_providers.tf
+++ terraform_providers.tf
@@ -5,15 +5,24 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=4.79.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.9.0"
+    }
   }
 }
 
 ## This code block defines the Azure Resource Manager provider.
 provider "azurerm" {
-  features {}
-  
-  subscription_id       = var.subscription_id
-  client_id             = var.client_id
-  client_secret         = var.client_secret
-  tenant_id             = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false # This is to handle MCAPS or other policy driven resource creation.
+    }
+  }
+  resource_provider_registrations = "extended"
 }
