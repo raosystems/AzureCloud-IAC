21d6af6e92b117c5142534938b756e5ca12a19dd
Amit Kumar <amitrao2026ce@hotmail.com>
2026-07-01 10:24:14 +0530

    removed CreatedDate TAG value from locals.tf and other code files

--- locals.tf
+++ locals.tf
@@ -1,13 +1,13 @@
 locals {
   common_tags = {
-    CostCenter                 = var.CostCenter
-    CreatedBy                  = var.CreatedBy
-    Environment                = var.Environment-Prod
-    ResourceOwner              = var.ResourceOwner
-    SupportTeam-CloudEngg      = var.SupportTeam-CloudEnggTeam
-    ManagedBy                  = var.ManagedBy
-    CreatedDate                = formatdate("DD-MMM-YYYY", timestamp())
-    ResourceLocation-EastUs2   = var.ResourceLocation_EastUs2
+    CostCenter            = var.CostCenter
+    CreatedBy             = var.CreatedBy
+    Environment           = var.Environment-Prod
+    ResourceOwner         = var.ResourceOwner
+    SupportTeam-CloudEngg = var.SupportTeam-CloudEnggTeam
+    ManagedBy             = var.ManagedBy
+    #CreatedDate              = formatdate("DD-MMM-YYYY", timestamp())
+    ResourceLocation-EastUs2 = var.ResourceLocation_EastUs2
     #ResourceLocation_CentralUS = var.ResourceLocation_CentralUs
   }
 }
